44d41242fd9e704e8850dd2ba3d4689cd5ab63dd
moller.peter <moller.g.peter@gmail.com>
2015-08-05 16:24:21 +0200

    work in progress

--- public/js/controllers/matchController.js
+++ public/js/controllers/matchController.js
@@ -1,15 +1,22 @@
 //"myAppName" controller.
-app.controller("matchController", ["$scope", "$routeParams", "Match", "Region, Team", function($scope, $routeParams, Match, Region, Team){
+app.controller("matchController", ["$scope", "$routeParams", "Match", "Region", "Team", function($scope, $routeParams, Match, Region, Team){
 
   // var getLjusdal = Region.get();
   // console.log("getLjusdal: ", getLjusdal);
-
+  console.log("1: ");
   // collection.Region hämta regionPath som är lika med routeParams.region
   Region.get({regionPath: $routeParams.region}, function(answer){
-    console.log("answer: regionsnamn:", answer[0]._id);
+    // console.log("answer: regionsnamn:", answer[0]._id);
+  console.log("2: ");
+    var regionId = answer[0]._id;
 
-    Team.get({}, function(answer){
-      console.log("answer: regionsnamn:", answer[0]._id);
+    Team.get({
+      regionId: regionId,
+      division: $routeParams.division
+    },
+      function(gurka){
+      console.log("3: ");
+      console.log("gurka: regionsnamn:", gurka);
     });
   });
 
