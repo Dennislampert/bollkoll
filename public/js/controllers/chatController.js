0dbdc5c94c87d5c75cb6b6baaa8bf6cb28237b54
santinopetrovic <santinopetrovic@gmail.com>
2015-08-05 12:20:38 +0000

    chatcontroller and chat.html

---
+++ public/js/controllers/chatController.js
@@ -0,0 +1,27 @@
+app.controller("chatController", ["$scope", "Chat", function($scope, Chat){
+$scope.test = Chat.get();
+
+console.log($scope.test)
+
+  // $scope.test = Chat.create({
+  // 	message: {
+	 //  tag: "tag",
+  //     content: "daaaaaamp"
+  //   },
+  //   user: {
+  //     name: "Dampbarn",
+  //     loginProtection: true
+  //   },
+  //   status: {
+  //     goal: false,
+  //     endedGame: false
+  //   },
+  //   matchId: "41241"
+  // }, function() {
+  //   $scope.test = Chat.get();
+  // });
+
+  //   $scope.test = Chat.remove();
+  // });
+
+}]);
