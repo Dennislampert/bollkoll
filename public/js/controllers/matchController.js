60b7e2be986362807743792b29889bfd0c078027
moller.peter <moller.g.peter@gmail.com>
2015-08-05 14:26:50 +0200

    playing around

--- public/js/controllers/matchController.js
+++ public/js/controllers/matchController.js
@@ -1,29 +1,13 @@
 //"myAppName" controller.
 app.controller("matchController", ["$scope","Match", function($scope,Match){
 
-  $scope.createMatch = function() {
+  // $scope.createMatch = function() {
 
-    Match.create({
+  $scope.test = Match.get();
 
-      homeTeamId: 1,
-      guestTeamId: 1,
-      time: 1,
-      date: 1,
-      homeResults: 1,
-      guestResults: 1,
-      finishedGame: false,
-      regionId: 1,
-      divisionId: 1,
-      place: "Somewhere",
-      gameWinnerId: 1
-
-    }, function() {
-      $scope.test = Match.get();
-    });
-
-};
+// };
 
   // $scope.test = Matches.getById("55815ab22a32d551d4c54bd7");
-  // $scope.test = Matches.get({_id:"55815ab22a32d551d4c54bd7"});
+  // $scope.test = Matches.get({_id:"55c1f49292efcc982c00d36e"});
 
 }]);
