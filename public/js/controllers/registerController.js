07191e3a03d0ad94bd634d05e674b76b138a42fc
sebastianborjesson <sebastian.borjesson@live.se>
2015-08-07 15:40:31 +0200

    able to register user

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -1,17 +1,10 @@
 app.controller("registerController", ["$scope", "$http", "User", function($scope, $http, User) {
+  
+  $scope.userInfo = {};
   $scope.register = function() {
-    $scope.userInfo = {};
-    if ($scope.userInfo.password === $scope.userInfo.passwordCompare) {
-      User.create({
-        username: $scope.userInfo.username,
-        password: $scope.userInfo.password,
-        fname: $scope.userInfo.fname,
-        lname: $scope.userInfo.lname,
-        email: $scope.userInfo.email,
-        picturePath: "/uploads/images/default.jpeg"
-      });
-    } else {
-      return false;
-    }
+    
+    User.create($scope.userInfo, function(data) {
+      console.log("rr", data);
+    });
   };
 }]);
