f17475771db7fc967bcffcaf20d2dffea97d6034
moller.peter <moller.g.peter@gmail.com>
2015-08-11 13:42:43 +0200

    work in progress - trying to get results visible

--- public/js/controllers/matchStatusController.js
+++ public/js/controllers/matchStatusController.js
@@ -1,5 +1,20 @@
 app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Region", "Team", function($scope, $routeParams, Match, Region, Team){
 
+	Match.get({
+		homeTeamId: $scope.homeTeam._id,
+    guestTeamId: $scope.guestTeam._id,
+    place: $scope.match.place,
+    date: $scope.match.date,
+    // time: $scope.match.time,
+    homeResults: 0,
+    guestResults: 0,
+    finishedGame: 0
+    // regionId: regionAndDivisionId.regionId,
+    // division: regionAndDivisionId.division
+	});
+
+
+
 
 	$scope.saveResults = function(){
 		console.log("saving(updating) results! ", $scope);
