970290a67a46149a35dbc2d9df9ad7fe88eb8b34
santinopetrovic <santinopetrovic@gmail.com>
2015-08-11 07:38:20 +0000

    commit before merge

--- mongoose-schema/message.js
+++ mongoose-schema/message.js
@@ -2,7 +2,8 @@ module.exports = function(mongoose) {
   var messageSchema = mongoose.Schema({
 	// userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
 	content: {type: String, required: true},
-	// date: {type: Number, required:true}
+	date: { type : Date, default: Date.now },
+	status: {type: Boolean, default: false}
   });
   return mongoose.model("Message", messageSchema);
-}
+};
