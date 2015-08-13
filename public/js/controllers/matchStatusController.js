ee6d5b9c60a873807772d601eefe01ed72fbde1b
moller.peter <moller.g.peter@gmail.com>
2015-08-13 11:16:02 +0200

    UPDATE: syntax for matchStatus updated!

--- public/js/controllers/matchStatusController.js
+++ public/js/controllers/matchStatusController.js
@@ -2,38 +2,44 @@ app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Reg
 
     // console.log("$routeparams: ", $routeParams);
 
-    var populateObject = {};
+    // var populateObject = {};
 
-    populateObject._id = $routeParams.gameId;
+    $scope.match = Match.getById({
+        _id: $routeParams.gameId,
+        _populate: "homeTeamId guestTeamId"
+    });
 
-    populateObject._populate = "homeTeamId guestTeamId";
+    // populateObject._id = $routeParams.gameId;
+
+    // populateObject._populate = "homeTeamId guestTeamId";
 
     // När det finns tid, gör en GET för endast nödvändig data (results) för minskad datatrafik
-    function getUpdatedResults(){
+    /*function getUpdatedResults(){
         Match.get(
             populateObject, function(teams){
                 console.log("teams: ", teams);
                 $scope.teams = teams;
             });
     }
+*/
 
-
-    getUpdatedResults();
+    //getUpdatedResults();
 
 	$scope.saveResults = function(){
 		console.log("saving(updating) results! ", $scope);
-
-		Match.update({
-      _id: $routeParams.gameId
-    },
-    {
-      homeResults: $scope.match.homeResults,
-      guestResults: $scope.match.guestResults
-    });
-        getUpdatedResults();
+        $scope.match.$update(function(data) {
+            //getUpdatedResults();
+        });
+        /*
+        Match.update({
+          _id: $routeParams.gameId
+        },
+        {
+          homeResults: $scope.match.homeResults,
+          guestResults: $scope.match.guestResults
+        });
+        */
 	};
-
-    $scope.match = {homeResults:0,guestResults:0};
 
     $scope.increaseGoals = function (match,prop){match[prop]++;};
     $scope.decreaseGoals = function (match,prop){match[prop]--;};
