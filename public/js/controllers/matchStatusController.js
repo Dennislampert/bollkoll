55b1afec969d3e48510000d149f00540ba4a6e57
moller.peter <moller.g.peter@gmail.com>
2015-08-17 13:46:02 +0200

    WORK IN PROGRESS: styling in general

--- public/js/controllers/matchStatusController.js
+++ public/js/controllers/matchStatusController.js
@@ -1,9 +1,5 @@
 app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Region", "Team", function($scope, $routeParams, Match, Region, Team){
 
-    // console.log("$routeparams: ", $routeParams);
-
-    // var populateObject = {};
-
     function duplicateResults(){
          $scope.oldResults = {
             homeResults: $scope.match.homeResults,
@@ -17,36 +13,10 @@ app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Reg
     },duplicateResults);
 
 
-
-    // populateObject._id = $routeParams.gameId;
-
-    // populateObject._populate = "homeTeamId guestTeamId";
-
-    // När det finns tid, gör en GET för endast nödvändig data (results) för minskad datatrafik
-    /*function getUpdatedResults(){
-        Match.get(
-            populateObject, function(teams){
-                $scope.teams = teams;
-            });
-    }
-*/
-
-    //getUpdatedResults();
-
 	$scope.saveResults = function(){
 		console.log("saving(updating) results! ", $scope);
 
-
         $scope.match.$update(duplicateResults);
-        /*
-        Match.update({
-          _id: $routeParams.gameId
-        },
-        {
-          homeResults: $scope.match.homeResults,
-          guestResults: $scope.match.guestResults
-        });
-        */
 	};
 
     $scope.increaseGoals = function (match,prop){match[prop]++;};
