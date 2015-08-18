c393af95f54831d91dbf605321a88f184d391e0d
moller.peter <moller.g.peter@gmail.com>
2015-08-18 14:17:57 +0200

    WORKING: chat is receiving match results

--- public/js/controllers/matchStatusController.js
+++ public/js/controllers/matchStatusController.js
@@ -1,4 +1,4 @@
-app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Region", "Team", function($scope, $routeParams, Match, Region, Team){
+app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Region", "Team", "Message", "Login", function($scope, $routeParams, Match, Region, Team, Message, Login){
 
     function duplicateResults(){
          $scope.oldResults = {
@@ -14,10 +14,29 @@ app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Reg
 
 
 	$scope.saveResults = function(){
-		console.log("saving(updating) results! ", $scope);
 
         $scope.match.$update(duplicateResults);
+
+        // creating an object where we, inside,  save content for the chat
+        $scope.chatInfo = {};
+
+        // putting results into the newly created object "chatInfo"
+        $scope.chatInfo.homeResults = $scope.match.homeResults;
+        $scope.chatInfo.guestResults = $scope.match.guestResults;
+
+        // the following syntax ends up visible in the chat
+        $scope.chatInfo.content = $scope.match.homeTeamId.name + " " + $scope.match.homeResults + " - " + $scope.match.guestTeamId.name + " " + $scope.match.guestResults;
+
+        $scope.chatInfo.userId = Login.user._id;
+        $scope.chatInfo.status = true;
+
+        console.log("blabla", $scope.chatInfo);
+
+        Message.create($scope.chatInfo, function(resultsInChat) {
+          console.log("resultsInChat", resultsInChat);
+        });
 	};
+
 
     $scope.increaseGoals = function (match,prop){match[prop]++;};
     $scope.decreaseGoals = function (match,prop){match[prop]--;};
@@ -25,4 +44,14 @@ app.controller("matchStatusController", ["$scope", "$routeParams", "Match", "Reg
     $scope.increaseDisallowed = function(x){return x > 30;};
 
 
+
+
+    // $scope.chatInfo = {};
+    //   $scope.send = function() {
+    //     $scope.chatInfo.userId = Login.user._id;
+    //     console.log("c", $scope.chatInfo);
+    //     Message.create($scope.chatInfo, function(data) {
+    //       console.log("lolek", data);
+    //     });
+    //   };
 }]);
