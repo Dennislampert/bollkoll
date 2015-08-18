cbcd97e4377c580e60e92281674021156ae9f8ed
Jallnnn <Jallnnn@Jallnnns-MacBook-Air.local>
2015-08-18 12:00:29 +0200

    Userprofile and image now working. Simple layout

--- public/js/controllers/profileController.js
+++ public/js/controllers/profileController.js
@@ -1,8 +1,9 @@
-app.controller("profileController", ["$scope", "$http", "$location", "FileUploader", "Login", function($scope, $http, $location, FileUploader, Login) {
+app.controller("profileController", ["$scope", "$routeParams", "File", "FileUploader", "Login", "User", function($scope, $routeParams, File, FileUploader, Login, User) {
 
   // reference(!) to Login.user object
   // (logged in user data)
-  $scope.user = Login.user;
+  //$scope.user = Login.user;
+  console.log(Login);
 
   $scope.files = [];
   $scope.upload = function() {
@@ -11,5 +12,34 @@ app.controller("profileController", ["$scope", "$http", "$location", "FileUpload
       $scope.uploadedFilePath = data;
     });
   };
+
+  console.log("route: ", $routeParams.username);
+
+    User.get({username: $routeParams.username}, function(userprofile){
+        $scope.user = userprofile[0];
+        File.get({owner:userprofile[0]._id}, function(Getfile){
+            $scope.image = Getfile.pop();
+            console.log("getfile: ", Getfile.pop());
+        console.log("user: ", userprofile);
+        });
+    });
+
+
+
+ //  function checkUser(){
+
+ //    if (Login.user._id){
+ //        console.log("userid", Login.user._id);
+
+	//	File.get({owner:Login.user._id}, function(Getfile){
+	//		$scope.image = Getfile.pop();
+	//		console.log("getfile: ", Getfile.pop());
+
+	//	});
+	// }else{
+ //        setTimeout(checkUser, 100);
+ //    }
+ //  }
+ //  checkUser();
 
 }]);
