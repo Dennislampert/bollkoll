fc1ba495f2de529a51bc72bb60358472cb9fe41d
sebastianborjesson <sebastian.borjesson@live.se>
2015-08-20 10:53:07 +0200

    modal complete

--- public/js/controllers/alertBoxController.js
+++ public/js/controllers/alertBoxController.js
@@ -1,35 +1,15 @@
-app.controller("alertBoxController", ["$scope", "$rootScope", "$modal", "$log", "$location", "Login", function($scope, $rootScope, $modal, $log, $location, Login) {
+app.controller("alertBoxController", ["$scope", "$log", "Login", "modalService", function($scope, $log, Login, modalService) {
+  if(!Login.user._id) {
 
-  $scope.open = function (size) {
+    $scope.openModal = function (size) {
 
-    var modalInstance = $modal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'alertbox.html',
-      controller: 'alertBoxController',
-      size: size,
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  var history = [];
-
-  $rootScope.$on('$routeChangeSuccess', function() {
-      history.push($location.$$path);
-  });
-
-  $rootScope.back = function () {
-      var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
-      $location.url(prevUrl);
-  };
+      var modalInstance = modalService.open({size:size});
+    };
+  }
 
 }]);
 
-app.controller('ModalInstanceCtrl',["$scope", "$modalInstance", function ($scope, $modalInstance) {
+app.controller('ModalInstanceCtrl',["$scope", "$rootScope", "$modalInstance", "$location", function ($scope, $rootScope, $modalInstance, $location) {
 
   $scope.redirect = function () {
     $modalInstance.close();
@@ -38,5 +18,35 @@ app.controller('ModalInstanceCtrl',["$scope", "$modalInstance", function ($scope
 
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
+    var history = [];
+
+    $rootScope.$on('$routeChangeSuccess', function() {
+        history.push($location.$$path);
+    });
+
+    $rootScope.back = function () {
+        var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+        $location.url(prevUrl);
+    };
   };
 }]);
+
+//function to open the modal if no user
+
+// $scope.$on('$routeChangeSuccess', function() {
+
+//     if(!Login.user._id) {
+//       modalService.open({size:300});
+//     }
+//   });
+
+
+// resolve function to load the user before the app kicks in
+
+// resolve: {
+//           getUser: ['$http', 'Login', function ($http, Login) {
+//             $http.get('api/login').then(function(user) {
+//               return user;
+//             });
+//           }]
+//       }
