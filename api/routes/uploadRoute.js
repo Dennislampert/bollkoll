cf10cebdca25a3bb366d99c12cd1345e1eec4ed6
Jallnnn <julle81@hotmail.com>
2015-08-24 11:04:51 +0200

    Changed from lwip to gm.

--- api/routes/uploadRoute.js
+++ api/routes/uploadRoute.js
@@ -3,7 +3,7 @@ var path = require('path'),
     fs = require('fs'),
     // include the multipart middleware for file uploading
     multipart = require('connect-multiparty'),
-    imagehandler = require('lwip');
+    gm = require('gm');
 
 
 
@@ -28,46 +28,39 @@ module.exports = function(mongoose) {
       console.log("filnamn: ", file.name.split(".").pop());
 
       // decide where to store the file
+        
       var uploadPath = path.normalize(config.upload.publicPath + file.name);
+      var publicPath = '/' + path.relative(process.cwd() + '/public', uploadPath);
 
       var filetype = file.name.split(".").pop();
+      console.log("uploadpath :", uploadPath);
+      gm(data)
+      .resize(200,200)
+      .crop(200, 200)
+      .write(uploadPath, function(err){
+  
+        // get the mongoose 'File' model
+        var FileModel = mongoose.model("File");
 
-      imagehandler.open(data, filetype, function(err, image){
-        // check err...
-        // define a batch of manipulations and save to disk as JPEG:
-            console.log("hejhej :", image);
-            console.log("uploadpath :", uploadPath);
-        image.batch()
-          .resize(200,200)
-          .crop(200, 200)       // crop a 200X200 square from center
-          .writeFile(uploadPath, function(err){
-            // check err...
-            // done.
-          // find public path (for <img src=""> tags etc)
-          var publicPath = '/' + path.relative(process.cwd() + '/public', uploadPath);
-
-          // get the mongoose 'File' model
-          var FileModel = mongoose.model("File");
-
-          // create a new File document
-          var dbFile = new FileModel({
-            name: file.name,
-            path: publicPath,
-            type: file.type,
-            owner: req.session.user // all files have an owner
-          });
-          
-          // save file info to mongodb
-          dbFile.save(function(err, data) {
-            if (err) { throw err; }
-            // and finally send a response to client
-            res.json(publicPath);
-            scaleimage("public"+publicPath);
-          });
+        // create a new File document
+        var dbFile = new FileModel({
+          name: file.name,
+          path: publicPath,
+          type: file.type,
+          owner: req.session.user // all files have an owner
+        });
+        
+        // save file info to mongodb
+        dbFile.save(function(err, data) {
+          if (err) { throw err; }
+          // and finally send a response to client
+          res.json(publicPath);
+          scaleimage("public"+publicPath);
         });
       });
-
-
     });
+
+
+
   }];
 };
