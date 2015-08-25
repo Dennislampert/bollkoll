e4b68ac1997e2a119a29300ac04df2c119898d36
sebastianborjesson <sebastian.borjesson@live.se>
2015-08-25 09:52:10 +0200

    working on logout

--- public/js/controllers/headerController.js
+++ public/js/controllers/headerController.js
@@ -1,6 +1,6 @@
-app.controller("headerController", 
-  ["$scope", "$rootScope", "Login",
-  function($scope, $rootScope, Login) {
+app.controller("headerController",
+  ["$scope", "$rootScope", "Login", "$window", "modalService",
+  function($scope, $rootScope, Login, $window, modalService) {
   $scope.headerUser = Login.getCurrentUser();
   $scope.nav  = {
     collapsed: true
@@ -12,11 +12,18 @@ app.controller("headerController",
     $scope.navText = data || origNavText;
   });
 
-  $rootScope.$on('login', function() { 
+  $rootScope.$on('login', function() {
     $scope.headerUser = Login.getCurrentUser();
   });
 
   $scope.collapseToggle = function() {
     $scope.nav.collapsed = !$scope.nav.collapsed;
+  };
+
+  $scope.logout = function() {
+    Login.logout();
+    modalService.open({
+      templateUrl: 'partials/logoutAlert.html',
+    })
   };
 }]);
