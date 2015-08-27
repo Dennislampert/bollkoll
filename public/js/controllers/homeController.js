a5b40cb6b0b0254d539f1d3c4007b064f62c4751
TKatra <tobiaskatra@hotmail.com>
2015-08-27 11:17:14 +0200

    The site now remembers the settings for region and divison.

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,6 +1,17 @@
 //"myAppName" controller.
 app.controller("homeController",
-  ["$scope", "NavTitleChange",
-  function($scope, NavTitleChange) {
+  ["$scope", "NavTitleChange", "UserStore",
+  function($scope, NavTitleChange, UserStore) {
   NavTitleChange("Hem");
+  var userStoreKeys = [
+    "regionDropDown",
+    "divisionDropDown"
+  ]
+  userStoreKeys.forEach(function(key) {
+    $scope[key] = UserStore.temp[key] ? UserStore.temp[key] : undefined;
+  });
+
+  $scope.updateUserStore = function(key) {
+    UserStore.temp[key] = $scope[key];
+  }
 }]);
