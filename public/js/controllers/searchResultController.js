c504b83ce5d7fb6f617e52f1d4d5199750f23087
santinopetrovic <santinopetrovic@gmail.com>
2015-08-31 12:41:04 +0000

    Searchparams without "or".

--- public/js/controllers/searchResultController.js
+++ public/js/controllers/searchResultController.js
@@ -1,9 +1,23 @@
 app.controller("searchResultController",
-  ["$http", "$scope", "$routeParams", "User", "Region",
-  function($http, $scope, $routeParams, User, Region) {
+  ["$http", "$scope", "$routeParams", "User", "Region", "Match",
+  function($http, $scope, $routeParams, User, Region, Match) {
   	console.log("params", $routeParams.searchParams);
-  	var userParams = {username:$routeParams.searchParams};
-    var regionParams = {regionName:$routeParams.searchParams};
+    var searchRegExp = new RegExp(".*" + $routeParams.searchParams + ".*");
+    
+    // This should work just fine bug in Mongresto (Thomas will fix)
+/*  	var userParams = {
+      $or: [
+        {username:searchRegExp},
+        {fname:searchRegExp},
+        {lname:searchRegExp}
+      ]
+    };*/
+    // Temporary while mongresto bug
+    searchParams = {_all: searchRegExp};
+    
+    // var regionParams = {
+    //   regionName:$routeParams.searchParams
+    // };
   	// for (var i in $routeParams){
    //    if (i == "username") {
    //      searchParams[i] = new RegExp($routeParams[i], "i");
@@ -14,12 +28,16 @@ app.controller("searchResultController",
 
   	$scope.allSearchResults = [];
   	$scope.searchResult = {};
-  	User.get(userParams, function(result){
+  	User.get(searchParams, function(result){
         console.log("lol");
         $scope.allSearchResults.push(result);
         console.log("$scope.allSearchResults: ", $scope.allSearchResults);
     });
-    Region.get(regionParams,function(result){
+    Region.get(searchParams,function(result){
+        $scope.allSearchResults.push(result);
+        console.log("$scope.allSearchResults: ", $scope.allSearchResults);
+    });
+    Match.get(searchParams,function(result){
         $scope.allSearchResults.push(result);
         console.log("$scope.allSearchResults: ", $scope.allSearchResults);
     });
