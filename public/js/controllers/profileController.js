d1b9910490e06dcac59a4a41b1185d1230bf0da9
moller-g-peter <moller.g.peter@gmail.com>
2015-09-05 20:02:14 +0200

    send me back to user

--- public/js/controllers/profileController.js
+++ public/js/controllers/profileController.js
@@ -8,49 +8,49 @@ app.controller("profileController",
     
   var stop = true;
 
-  $scope.upload = function() {
-    if (stop === false){
-      User.get({username: $routeParams.username}, function(userprofile){
-      FileUploader($scope.files).success(function(data) {
-        console.log("saved files, public path: ", data);
-        $scope.uploadedFilePath = data;
-      });
-    });
-    }
-  };
+  // $scope.upload = function() {
+  //   if (stop === false){
+  //     User.get({username: $routeParams.username}, function(userprofile){
+  //     FileUploader($scope.files).success(function(data) {
+  //       console.log("saved files, public path: ", data);
+  //       $scope.uploadedFilePath = data;
+  //     });
+  //   });
+  //   }
+  // };
 
-  $scope.$watch('files', function (file) {
+  // $scope.$watch('files', function (file) {
     
-    if (file){
-      console.log("file: ",file);
-      if (file.length){
-        var fileType = file[0].name.split('.').pop().toLowerCase();
-        if (fileType == "jpg" || fileType == "png"){
-          stop = false;
-          console.log("uploaded image is okey");
-        }else{
-          // send bastis modual
+  //   if (file){
+  //     console.log("file: ",file);
+  //     if (file.length){
+  //       var fileType = file[0].name.split('.').pop().toLowerCase();
+  //       if (fileType == "jpg" || fileType == "png"){
+  //         stop = false;
+  //         console.log("uploaded image is okey");
+  //       }else{
+  //         // send bastis modual
 
-          $scope.errorbox = modalService.open({
-            templateUrl:'partials/globalalert.html',
-            controller: 'uploadAlertController',
-            resolve: {
-              message: function() {
-                $scope.message = {};
-                $scope.message.header = "Bilduppladdning misslyckades!";
-                $scope.message.msg = "Vi stöder inte detta bildformatet, vänligen ladda upp en bild med formatet png, jpg eller jpeg.";
-                return $scope.message;
-              }
-            },
-            close: function(closeData) {
-              $scope.files = [];
-              console.log("the modal closed, and sent back ", closeData);
-            }
-           });
-        }
-      }
-    }
-  });
+  //         $scope.errorbox = modalService.open({
+  //           templateUrl:'partials/globalalert.html',
+  //           controller: 'uploadAlertController',
+  //           resolve: {
+  //             message: function() {
+  //               $scope.message = {};
+  //               $scope.message.header = "Bilduppladdning misslyckades!";
+  //               $scope.message.msg = "Vi stöder inte detta bildformatet, vänligen ladda upp en bild med formatet png, jpg eller jpeg.";
+  //               return $scope.message;
+  //             }
+  //           },
+  //           close: function(closeData) {
+  //             $scope.files = [];
+  //             console.log("the modal closed, and sent back ", closeData);
+  //           }
+  //          });
+  //       }
+  //     }
+  //   }
+  // });
 
 
   function loadImage(){
@@ -66,6 +66,10 @@ app.controller("profileController",
 
 
   loadImage();
+  
+  $scope.editProfile = function(){
+    $location.url("/anvandare/" + $routeParams.username + "/settings");
+  };
 
 }]);
 
