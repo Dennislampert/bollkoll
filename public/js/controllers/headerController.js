0247532200911a29534de0e02bd59dd16afecd17
sebastianborjesson <sebastian.borjesson@live.se>
2015-09-07 14:36:52 +0200

    navbar fix

--- public/js/controllers/headerController.js
+++ public/js/controllers/headerController.js
@@ -1,30 +1,35 @@
 app.controller("headerController",
-  ["$scope", "$location", "$rootScope", "Login", "$window", "modalService", "$route",
-  function($scope, $location, $rootScope, Login, $window, modalService, $route) {
+  ["$scope", "$location", "$rootScope", "Login", "$window", "modalService",
+  function($scope, $location, $rootScope, Login, $window, modalService) {
+  $scope.headerUser = Login.getCurrentUser();
+  $scope.nav  = {
+    collapsed: true
+  };
+
+  var origNavText = 'Hem';
+  $scope.navText = origNavText;
+  $scope.$on("titleChange", function(e, data) {
+    $scope.navText = data || origNavText;
+  });
+
+  $rootScope.$on('login', function() {
     $scope.headerUser = Login.getCurrentUser();
-    $scope.nav  = {
-      collapsed: true
-    };
-    Login.protectRoute();
-    var origNavText = 'Hem';
-    $scope.navText = origNavText;
-    $scope.$on("titleChange", function(e, data) {
-      $scope.navText = data || origNavText;
-    });
+  });
 
-    $rootScope.$on('login', function() {
-      $scope.headerUser = Login.getCurrentUser();
-    });
+  $rootScope.$on('logout', function() {
+    $scope.headerUser = Login.getCurrentUser();
+  });
 
-    $rootScope.$on('logout', function() {
-      $scope.headerUser = Login.getCurrentUser();
-    });
-    $scope.logout = function() {
-      Login.logout();
-    };
-    $scope.search = function(seachTerm) {
-      if (seachTerm) {
-        $location.path("/search/" + seachTerm);
-      }
-    };
+  $scope.collapseToggle = function() {
+    $scope.nav.collapsed = !$scope.nav.collapsed;
+  };
+
+  $scope.logout = function() {
+    Login.logout();
+  };
+  $scope.search = function(seachTerm) {
+    if (seachTerm) {
+      $location.path("/search/" + seachTerm);
+    }
+  };
 }]);
